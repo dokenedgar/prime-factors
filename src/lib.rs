84af6a61aa58bb4f60b1231f2116290e0b29f42c
Doken Edgar <dokenedgarjr@gmail.com>
2025-01-24 16:30:07 +0100

    Added code to get the prime factors of a number

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+    let mut prime_factors: Vec<u64> = Vec::new();
+    let mut divisor: u64 = 2;
+    let mut quotient = n;
+    if n == 1 {
+        return prime_factors;
+    } else if n == 2 || n == 3 {
+        prime_factors.push(n);
+        return prime_factors;
+    }
+    loop {
+        if divisor == n {
+            break;
+        } else {
+            if quotient % divisor == 0 {
+                quotient = quotient / divisor;
+                prime_factors.push(divisor);
+            } else {
+                divisor += 1;
+            }
+        }
+    }
+    prime_factors
 }
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let result = prime_factors::factors(2);
+    println!("Prime factors: {:?}", result);
+}
