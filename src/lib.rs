8f1594fd2f3371ccd2ed9f0ecdd833b99dfeaf65
Doken Edgar <dokenedgarjr@gmail.com>
2025-01-29 16:33:36 +0100

    Fix large test taking long time

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,22 @@
-pub fn factors(n: u64) -> Vec<u64> {
+pub fn factors(mut n: u64) -> Vec<u64> {
     let mut prime_factors: Vec<u64> = Vec::new();
     let mut divisor: u64 = 2;
-    let mut quotient = n;
-    if n == 1 {
-        return prime_factors;
-    } else if n == 2 || n == 3 {
-        prime_factors.push(n);
-        return prime_factors;
-    }
+
     loop {
-        if divisor == n {
-            break;
-        } else {
-            if quotient % divisor == 0 {
-                quotient = quotient / divisor;
-                prime_factors.push(divisor);
-            } else {
-                divisor += 1;
+        if n > 1 {
+            loop {
+                if n % divisor == 0 {
+                    prime_factors.push(divisor);
+                    n /= divisor;
+                } else {
+                    break;
+                }
             }
+            divisor += 1;
+        } else {
+            break;
         }
     }
+
     prime_factors
 }
